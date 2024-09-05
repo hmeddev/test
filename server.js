2ad62ac1b9686165e7da83d5274243ca60b417d1
Glitch (glitch-hello-node) <none>
2024-09-05 10:31:08 +0000

    💵👲 Checkpoint ./server.js:60829596/1336

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const redis = require('redis');
-const { RedisStore } = require('connect-redis'); 
 const csrf = require('csurf');
 const { loginLimiter } = require('./middlewares/rateLimit');
 const authRoutes = require('./routes/auth');
@@ -12,21 +10,9 @@ const userRoutes = require('./routes/user');
 
 // Firebase Admin SDK
 
-// إنشاء عميل Redis
-// const redisClient = Redis.createClient();
-
-// إعداد RedisStore باستخدام connect-redis
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const redisClient = redis.createClient({
-    host: 'redis-19105.c300.eu-central-1-1.ec2.redns.redis-cloud.com',  // أو رابط Redis إذا كان مستضافًا في مكان آخر
-    port: 19105          // المنفذ الافتراضي لـ Redis
-});
-redisClient.on('error', function (err) {
-    console.error('Could not connect to Redis', err);
-});
 
 // Middleware
 app.use(helmet());
@@ -34,11 +20,10 @@ app.use(cors({ origin: ['upbeat-hickory-expansion.glitch.me'], credentials: true
 app.use(cookieParser());
 app.use(express.json());
 app.use(session({
-    store: new RedisStore({ client: redisClient }),
-    secret: 'your_secret_key',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false, httpOnly: true, maxAge: 60000 }  // زيادة الأمان باستخدام httpOnly
+  secret: process.env.SESSION_SECRET || 'default_session_secret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false, httpOnly: true, maxAge: 60000 }
 }));
 
 // CSRF Protection
