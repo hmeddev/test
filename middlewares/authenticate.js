7fa8ae463122cf5fb4606461972552553bd14310
Glitch (glitch-hello-node) <none>
2024-09-06 12:13:43 +0000

    🚨🍣 Checkpoint ./middlewares/authenticate.js:60829596/379

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -12,11 +12,12 @@ const authenticate = (req, res, next) => {
   const token = authHeader; //.split(" ")[1]
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      console.log(err)
-      if (err.name === "TokenExpiredError") {
+      
+      if (err.name === "TokenExpiredError" || err.name === "jwt expired") {
+        console.log("expired")
         // إذا انتهت صلاحية التوكن، نعيد رسالة مناسبة
         return res.status(401).json({ status: false, error: "Token expired" });
-      }
+      }else
       return res.status(403).json({ status: false, error: "Invalid token." ,err });
     }
 
