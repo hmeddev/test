050230324885483d7b468710e1573ad03154026e
Glitch (glitch-hello-node) <none>
2024-09-23 14:23:28 +0000

    🚊🏙 Checkpoint ./validators/authValidator.js:60829596/1398

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -15,34 +15,42 @@ async function validateApiKey(key) {
 
 const { createErrorResponse, createSuccessResponse, main } = require('../Handler');
 
-const schema = Joi.object({
-  username: Joi.string()
-    .alphanum()
-    .min(5)
-    .max(30)
-    .required()
-    .pattern(/^(?!.*\.\.)(?!.*__)[a-zA-Z0-9._]*$/),
-  apikey: Joi.string()
-    .alphanum()
-    .min(5)
-    .max(30)
-    .required()
-    .pattern(/^(?!.*\.\.)(?!.*__)[a-zA-Z0-9._]*$/),
-  nickname: Joi.string()
-    .alphanum()
-    .min(5)
-    .max(30)
-    .required()
-    .pattern(/^(?!.*\.\.)(?!.*__)[a-zA-Z0-9._]*$/),
-  password: Joi.string()
-    .min(8)
-    .max(30)
-    .required()
-    .pattern(/^[a-zA-Z0-9!@#$%^&*()_+\-={}\[\]:;"'<>?,.\/\\|`~]*$/),
-});
+const schema = (isSignup) => {
+  const baseSchema = {
+    username: Joi.string()
+      .alphanum()
+      .min(5)
+      .max(30)
+      .required()
+      .pattern(/^(?!.*\.\.)(?!.*__)[a-zA-Z0-9._]*$/),
+    apikey: Joi.string()
+      .alphanum()
+      .min(5)
+      .max(30)
+      .required()
+      .pattern(/^(?!.*\.\.)(?!.*__)[a-zA-Z0-9._]*$/),
+    password: Joi.string()
+      .min(8)
+      .max(30)
+      .required()
+      .pattern(/^[a-zA-Z0-9!@#$%^&*()_+\-={}\[\]:;"'<>?,.\/\\|`~]*$/),
+  };
+
+  if (isSignup) {
+    baseSchema.nickname = Joi.string()
+      .alphanum()
+      .min(5)
+      .max(30)
+      .required()
+      .pattern(/^(?!.*\.\.)(?!.*__)[a-zA-Z0-9._]*$/);
+  }
+
+  return Joi.object(baseSchema);
+};
+
 
 async function validateRequest(req, res, next, isSignup = true) {
-  const { error } = schema.validate(req.body);
+  const { error } = schema(isSignup).validate(req.body);
   if (error) return res.status(400).json(createErrorResponse(error.details, isSignup ? 14 : 15));
 
   const isValidApiKey = await validateApiKey(req.body.apikey);
