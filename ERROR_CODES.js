a2606b61a891c09a5bde569218e2fe16ed03f9e4
Glitch (glitch-hello-node) <none>
2024-09-23 14:35:33 +0000

    🎿🐯 Checkpoint ./middlewares/rateLimit.js:60829596/3 ./ERROR_CODES.js:60829596/159

---
+++ ERROR_CODES.js
@@ -0,0 +1,8 @@
+const ERROR_CODES = {
+    INVALID_USER_ID: 1001,
+    DATABASE_ERROR: 1002,
+    PERMISSION_DENIED: 1003
+};
+
+// تصدير رموز الأخطاء
+module.exports = ERROR_CODES;
--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 25,
+  max: 5,
   message: 'Too many login attempts. Try again in 15 minutes.',
 });
 
