181983047d2bc147467ac649413db17832f207fb
Glitch (glitch-hello-node) <none>
2024-09-23 13:03:18 +0000

    🚆🌗 Checkpoint ./validators/authValidator.js:60829596/285

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -16,9 +16,9 @@ const validateSignup = (req, res, next) => {
 
 const validateLogin = (req, res, next) => {
   const schema = Joi.object({
-    username: Joi.string().alphanum().min(5).max(30).required(),
+    username: Joi.string().alphanum().min(5).max(30).required().pattern(/^(?!.*\.\.)(?!.*__)[a-zA-Z0-9._]*$/),
     apikey: Joi.string().alphanum().min(5).max(30).required().pattern(/^(?!.*\.\.)(?!.*__)[a-zA-Z0-9._]*$/),
-    password: Joi.string().min(8).max(30).required()
+    password: Joi.string().min(8).max(30).required().pattern(/^[a-zA-Z0-9!@#$%^&*()_+\-={}\[\]:;"'<>?,.\/\\|`~]*$/)
   });
 
   const { error } = schema.validate(req.body);
