1b89fca53c48dba13fec277958147664fce6ee63
Glitch (glitch-hello-node) <none>
2024-09-29 15:13:43 +0000

    👡👗 Checkpoint ./middlewares/rateLimit.js:60829596/2 ./middlewares/authenticate.js:60829596/87

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-
+const { createErrorResponse, createSuccessResponse, main } = require('../lib/Handler');
 const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: 2,
   message: 'Too many login attempts. Try again in 15 minutes.',
 });
 
