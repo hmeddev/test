cc0331e844d13ca38b5ebfefa74a7c50c157226d
Glitch (glitch-hello-node) <none>
2024-10-03 17:51:53 +0000

    🐄👹 Checkpoint ./routes/game.js:60829596/136 ./controllers/Shop.js:60829596/1666

---
+++ controllers/Shop.js
@@ -0,0 +1,41 @@
+// ShopController.js
+
+const admin = require('../firebase/firebase');
+const { createErrorResponse, createSuccessResponse,main } = require('../lib/Handler');
+const ERROR_CODES = require('../lib/errorCodes');
+const db = admin.database();
+const path = main().path;
+const getShop = (req, res) => {
+  console.log("Fetching Shop...");
+  const ShopRef = db.ref(path+'/items/');
+
+  ShopRef.once('value', snapshot => {
+    if (!snapshot.exists()) {
+      return res.status(404).json(createErrorResponse(ERROR_CODES.GAMES_NOT_FOUND, ERROR_CODES.GAMES_NOT_FOUND.message));
+    }
+
+    const Shop = snapshot.val();
+    
+    // تحويل البيانات إلى مصفوفة إذا لم تكن مصفوفة
+    const ShopArray = Array.isArray(Shop) ? Shop : Object.values(Shop);
+    
+    // إضافة تسلسل لكل لعبة
+    const ShopResponse = ShopArray.map((Shop, index) => ({
+      name: Shop.name || 'No name',
+      description: Shop.description || 'No description',
+      icon: Shop.icon || 'No image',
+      key: Shop.key || 'No key',
+      group: Shop.group || 'No group',
+      groupid: Shop.groupid || 'No groupid',
+      id: Shop.id || 'No id',
+      rarity: Shop.rarity || 'No rarity',
+      time: Shop.time || 'No time',
+      type: Shop.type || 'No type',
+      index: index + 1
+    }));
+
+    res.json(createSuccessResponse({ Shop: ShopResponse }, 'Shop fetched successfully.'));
+  });
+}
+  
+  module.exports = { getShop };
--- routes/game.js
+++ routes/game.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { getgames } = require('../controllers/games');
 const { getitems } = require('../controllers/items');
+const { getShop } = require('../controllers/Shop');
 const { authenticate } = require('../middlewares/authenticate');
 const router = express.Router();
 
 router.get('/games', authenticate, getgames);
 router.get('/items', authenticate, getitems);
+router.get('/Shop', authenticate, getShop);
 module.exports = router;
