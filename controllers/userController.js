d3ccd30fba389e0792543cd91f271449c8f21024
Glitch (glitch-hello-node) <none>
2024-10-07 12:30:36 +0000

    🎽🌧 Checkpoint ./lib/errorCodes.js:60829596/4613 ./controllers/userController.js:60829596/2895

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,36 +1,49 @@
 const admin = require('../firebase/firebase');
 const { createErrorResponse, createSuccessResponse, main } = require('../lib/Handler');
+const ERROR_CODES = require('../lib/ERROR_CODES');
 const db = admin.database();
 const path = main().path;
+
 // Update user controller
 const updateUser = (req, res) => {
   const { username } = req.body;
-  const userRef = db.ref(path+'/users/' + req.user.uid);
+  const userRef = db.ref(path + '/users/' + req.user.uid);
 
   userRef.once('value', snapshot => {
     if (!snapshot.exists()) {
-      return res.status(404).json({status:false, error: 'User not found.' });
+      // استخدام createErrorResponse مع رمز الخطأ الجديد
+      return res.status(404).json(createErrorResponse(ERROR_CODES.USER_NOT_FOUND, ERROR_CODES.USER_NOT_FOUND.message));
     }
 
-    userRef.update({ username }, () => {
-      res.json({status:true, message: 'User updated successfully!' });
+    userRef.update({ username }, (error) => {
+      if (error) {
+        // إذا حدث خطأ أثناء التحديث
+        return res.status(500).json(createErrorResponse(ERROR_CODES.FAILED_TO_UPDATE_USER, ERROR_CODES.FAILED_TO_UPDATE_USER.message));
+      }
+      // استخدام createSuccessResponse للرد عند النجاح
+      res.json(createSuccessResponse({}, 'User updated successfully!'));
     });
   });
 };
 
 // Get user controller
 const getUser = (req, res) => {
-  const userRef = db.ref(path+'/users/' + req.user.uid);
-console.log("user")
+  const userRef = db.ref(path + '/users/' + req.user.uid);
+
   userRef.once('value', snapshot => {
     if (!snapshot.exists()) {
-      return res.status(404).json({status:false, error: 'User not found.' });
-      console.log("user-error")
+      // استخدام createErrorResponse مع رمز الخطأ الجديد
+      return res.status(404).json(createErrorResponse(ERROR_CODES.USER_NOT_FOUND, ERROR_CODES.USER_NOT_FOUND.message));
     }
 
     const user = snapshot.val();
-    res.json({status:true, uid: user.uid, username: user.username, coin:1000000});
-    console.log("user-done")
+    // استخدام createSuccessResponse للرد عند النجاح
+    res.json(createSuccessResponse({
+      uid: user.uid,
+      username: user.username,
+      nickname: user.nickname,
+      coin: 1000000
+    }, 'User fetched successfully!'));
   });
 };
 
