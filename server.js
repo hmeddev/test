5a4d294331d864ddc6c8fc164f6d2abd1b3bf347
Glitch (glitch-hello-node) <none>
2024-10-07 17:37:50 +0000

    🐞🎢 Checkpoint ./server.js:60829596/160

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: ['upbeat-hickory-expansion.glitch.me'], credentials: true }));
+app.use(cors({ origin: ['bbs-game.glitch.me'], credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(session({
